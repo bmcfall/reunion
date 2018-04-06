31805a2340ee29fc374e51240e945d44fcbd11dd
Blythe McFall <mcfall.blythe@gmail.com>
2018-04-05 18:12:45 -0600

    Added modifications to the activity.rb and activity_test.rb files, and included my written responses in the Synthesis.txt file.

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,10 @@
 class Activity
+
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,15 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_has_a_name
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end
+
+  def test_it_has_a_hash
+    activity = Activity.new("hiking")
+
+    assert_equal Hash.new, activity.participants
+  end
 end
